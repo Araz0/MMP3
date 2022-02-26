1a4071cf0cbb40a1d704e17154cee00bb3351151
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-02-26 23:30:55 +0100

    cleaned up file upload

--- src/php/projects/create.php
+++ src/php/projects/create.php
@@ -4,17 +4,22 @@
 <html lang="en">
 
 <?php 
+// Show all information, defaults to INFO_ALL
+
 
     $pagetitle = "Create Project";
     require '../config.php'; //This is the oauth-config! You will also need your standard-config file
     require '../functions.php';
     require "../components/head.php";
-    if (isset($_POST['project_title'])) {
+    if (isset($_POST['create_project'])) {
         $project_title = $_POST['project_title'];
         $project_sufix = $_POST['project_sufix'];
         $project_subtitle = $_POST['project_subtitle'];
         $project_excerpt = $_POST['project_excerpt'];
         $project_description = $_POST['project_description'];
+        
+        fileUpload("storage", "project_thumbnail", array('jpeg','jpg','png'));
+        mutlipleFilesUpload("storage", "project_members_thumbnail", array('jpeg','jpg','png'));
 
         if (isset($_POST['project_members_name'])) {
             for ($i=0; $i < count($_POST['project_members_name']); $i++) { 
@@ -46,24 +51,8 @@
             'address' => $_POST['project_location_address']
         );
 
-        //define file names and other variables to make code easier to read
-        $upload_folder = "storage/";
-        $uploadDir = dirname( $_SERVER["DOCUMENT_ROOT"] ) . "/public/".$upload_folder;
-        $thumUpload = basename($_FILES['project_thumbnail']['name']);
-        $heroUpload = basename($_FILES['project_hero']['name']);
+        
 
-        $tmpThumName = basename($_FILES['project_thumbnail']['tmp_name']);
-        $tmpHeroName = basename($_FILES['project_hero']['tmp_name']);
-        $hashedThumName = md5($tmpThumName.$thumUpload).".jpg";
-        $hashedHeroName = md5($tmpHeroName.$heroUpload).".jpg";
-        $heroPath = $upload_folder.$hashedHeroName;
-        $thumbnailPath = $upload_folder.$hashedThumName;
-        $extensions = array( 'image/jpeg','image/jpg');
-        /*if (move_uploaded_file($_FILES['project_thumbnail']['tmp_name'], $uploadDir.$thumUpload) && move_uploaded_file($_FILES['project_hero']['tmp_name'], $uploadDir.$heroUpload)) {
-            echo "🚀🚀🚀🚀🚀🚀";
-        } else {
-            echo "❌❌❌❌";
-        }*/
     }
     
 ?>
@@ -75,31 +64,31 @@
     <form action="/projects/create.php" method="post" enctype="multipart/form-data">
         <label for="project_title"><b>project_title</b></label>
         <input type="text" name="project_title" value="my title" id="project_title" required>
-        <br>
+        
     
         <label for="project_sufix"><b>project_sufix</b></label>
         <input type="text" name="project_sufix" value="mysufix" id="project_sufix" required>
-        <br>
+        
 
         <label for="project_subtitle"><b>project_subtitle</b></label>
         <input type="text" name="project_subtitle" value="subtitle here" id="project_subtitle" required>
-        <br>
+        
 
         <label for="project_excerpt"><b>project_excerpt</b></label>
         <input type="text" name="project_excerpt" value="excerpt here of description" id="project_excerpt" required>
-        <br>
+        
 
         <label for="project_description"><b>project_description</b></label>
         <textarea name="project_description" id="project_description" rows="4" cols="50" required>project_description project_description project_description</textarea>
-        <br>
+        
 
         <label for="project_thumbnail"><b>project_thumbnail</b></label>
         <input type="file" name="project_thumbnail" id="project_thumbnail" accept="image/*,.jpg">
-        <br>
+        
 
         <label for="project_hero"><b>project_hero</b></label>
         <input type="file" name="project_hero" id="project_hero" accept="image/*,.jpg,video/mp4">
-        <br>
+        
         
         <div class="project_members_container" id="project_members_container">
             <div class="project_member_wrapper">
@@ -122,7 +111,7 @@
             <option value="Bachelor">Bachelor</option>
             <option value="Master">Master</option>
         </select>
-        <br>
+        
 
         <label for="project_category"><b>project_category</b></label>
         <select name="project_category" > 
@@ -133,11 +122,11 @@
             <option value="MMP3">MMP3</option>
             <option value="AbschlussProject">AbschlussProject</option>
         </select>
-        <br>
+        
 
         <label for="project_tags"><b>project_tags</b></label>
         <input type="text" name="project_tags" value="tag1, tag2, tag3" id="project_tags" required>
-        <br>
+        
 
         <div class="project_links_container" id="project_links_container">
             <div class="project_link_wrapper">
@@ -155,14 +144,14 @@
 
         <label for="project_location_type"><b>project_location_type</b></label>
         <select name="project_location_type" >
-                <option value="FH Room">FH Room</option>
-                <option value="Street Address">Street Address</option>
-            </select>
-        <br>
+            <option value="FH Room">FH Room</option>
+            <option value="Street Address">Street Address</option>
+        </select>
+        
 
         <label for="project_location_address"><b>project_location_address</b></label>
         <input type="text" name="project_location_address" value="355" id="project_location_address" required>
-        <br>
+        
 
         <input class="" type="submit" value='Create' name='create_project'>
     </form>
