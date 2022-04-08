dc6100ae90c8ffbda9098cbb1c265f800e41ce6e
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-04-09 00:22:23 +0200

    added main nav

--- public/components/nav.php
+++ public/components/nav.php
@@ -1,4 +1,16 @@
-<nav>
+<nav class="main-menu">
+
+    
+    <ul>
+        <?php if (isSet($_SESSION['fhsUser'])) echo $_SESSION['fhsUser']; ?>
+        <li><a href="/"><img src="media/logo-icon.png" alt="Rundgang Logo Icon"></a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Programm</a></li>
+        <li><a href="">Speeddating</a></li>
+        <li><a href="">Tickts</a></li>
+        <li><a href="">Projekte</a></li>
+        <li><a href="">About</a></li>
+    </ul>
     <ul>
         <?php 
         if (!isSet($_SESSION['fhsUser'])) { ?> 
@@ -6,6 +18,7 @@
         <?php }else { ?>
             <li><a href="/logout.php">logout</a></li>
         <?php } ?>
+        <li><a href="">O-</a></li>
+        <li><a href="">X</a></li>
     </ul>
-    <h1><?php echo $_SESSION['fhsUser']; ?></h1>
 </nav>
