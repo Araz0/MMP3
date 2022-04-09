1d8b7118acd76f6a53c34a4ae3add79537c0115e
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-04-10 01:55:41 +0200

    init captcha component

---
+++ src/php/components/captcha.php
@@ -0,0 +1,37 @@
+<div id="captcha" class="captcha-container">
+    <div id="captcha-header" class="captcha-row-spacebetween">
+        <span>Captcha</span>
+        <button id="captcha-close-btn">X</button>
+    </div>
+    <div class="images-Container">
+        <div class="images">
+            <input type="checkbox" id="bx1" name="bx1" value="bx1">
+            <label for="bx1"></label>
+            <input type="checkbox" id="bx2" name="bx2" value="bx2">
+            <label for="bx2"></label>
+            <input type="checkbox" id="bx3" name="bx3" value="bx3">
+            <label for="bx3"></label>
+            <input type="checkbox" id="bx4" name="bx4" value="bx4">
+            <label for="bx4"></label>
+            <input type="checkbox" id="bx5" name="bx5" value="bx5">
+            <label for="bx5"></label>
+            <input type="checkbox" id="bx6" name="bx6" value="bx6">
+            <label for="bx6"></label>
+            <input type="checkbox" id="bx7" name="bx7" value="bx7">
+            <label for="bx7"></label>
+            <input type="checkbox" id="bx8" name="bx8" value="bx8">
+            <label for="bx8"></label>
+            <input type="checkbox" id="bx9" name="bx9" value="bx9">
+            <label for="bx9"></label>
+        </div>
+        <img src="<?php echo $captcha_image_path; ?>" alt="Captcha image">
+    </div>
+    <div class="captcha-row-spacebetween">
+        <label class="custom-checkbox" for="notarobot">
+            <input type="checkbox" name="notarobot" id="notarobot">I'm not a robot</input>
+            <span class="custom-checkbox-box"></span>
+        </label>
+        <button id="captcha-next-btn">verify</button>
+    </div>
+    
+</div>
