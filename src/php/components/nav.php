ea480b1dba8d1ce8d672ede506a6a59a6a7d06d3
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-04-11 02:20:33 +0200

    added an easier layout of a mobile nav

--- src/php/components/nav.php
+++ src/php/components/nav.php
@@ -1,8 +1,17 @@
 <nav class="main-menu">
-    <button id="burgerButton">Menu</button>
+    <ul>
+        <li><a href="/"><img src="media/logo-icon.png" alt="Rundgang Logo Icon"></a></li>
+        <li><button id="burgerButton">Menu</button></li>
+        <?php 
+        if (!isSet($_SESSION['fhsUser'])) { ?> 
+            <li><a href="/auth/authorized.php">login</a></li>
+        <?php }else { ?>
+            <li><a href="/logout.php">logout</a></li>
+        <?php } ?>
+        <li><a href=""><img id="main-menu__search-icon" src="media/search-icon.png" alt="Search Icon"></a></li>
+    </ul>
     <ul>
         <?php if (isSet($_SESSION['fhsUser'])) echo $_SESSION['fhsUser']; ?>
-        <li><a href="/"><img src="media/logo-icon.png" alt="Rundgang Logo Icon"></a></li>
         <li><a href="">Home</a></li>
         <li><a href="">Programm</a></li>
         <li><a href="">Speeddating</a></li>
@@ -10,14 +19,15 @@
         <li><a href="">Projekte</a></li>
         <li><a href="">About</a></li>
     </ul>
+    
+</nav>
+<nav class="mobile-menu">
     <ul>
-        <?php 
-        if (!isSet($_SESSION['fhsUser'])) { ?> 
-            <li><a href="/auth/authorized.php">login</a></li>
-        <?php }else { ?>
-            <li><a href="/logout.php">logout</a></li>
-        <?php } ?>
-        <li><a href="">O-</a></li>
-        <li><a href="">X</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Programm</a></li>
+        <li><a href="">Speeddating</a></li>
+        <li><a href="">Tickts</a></li>
+        <li><a href="">Projekte</a></li>
+        <li><a href="">About</a></li>
     </ul>
 </nav>
