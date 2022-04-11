fefe435607020e255ffc2eafb6a32059cbe02ab6
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-04-11 02:18:33 +0200

    patched js to work with the elements for mobile nav

--- public/js/burgerMenuHandler.js
+++ public/js/burgerMenuHandler.js
@@ -1,12 +1,12 @@
 const burgerbutton = document.getElementById("burgerButton");
-const mainMenu = document.querySelector(".main-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 function toggleMenu() {
-    if (mainMenu.children[1].classList.contains("show-menu")) {
-        mainMenu.children[1].classList.remove("show-menu");
-        mainMenu.children[2].classList.remove("show-menu");
+    if (mobileMenu.classList.contains("show-menu")) {
+        mobileMenu.classList.remove("show-menu");
+        mobileMenu.classList.remove("show-menu");
     } else {
-        mainMenu.children[1].classList.add("show-menu");
-        mainMenu.children[2].classList.add("show-menu");
+        mobileMenu.classList.add("show-menu");
+        mobileMenu.classList.add("show-menu");
     }
 }
 
