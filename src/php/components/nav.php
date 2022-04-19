ceaeba4d5a49d969a2f5015168818a4900ea9356
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-04-20 00:22:27 +0200

    updated nav to be fully done

--- src/php/components/nav.php
+++ src/php/components/nav.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="/"><img src="media/logo-icon.png" alt="Rundgang Logo Icon"></a></li>
         <li><button id="burgerButton">Menu</button></li>
-        <li><a href=""><img id="main-menu__search-icon" src="media/search-icon.png" alt="Search Icon"></a></li>
+        <li><a href=""><img class="main-menu__search-icon" src="media/search-icon.png" alt="Search Icon"></a></li>
         <?php 
         if (!isSet($_SESSION['fhsUser'])) { ?> 
             <li><a href="/auth/authorized.php">login</a></li>
@@ -11,13 +11,19 @@
         <?php } ?>
     </ul>
     <ul>
-        <?php if (isSet($_SESSION['fhsUser'])) echo $_SESSION['fhsUser']; ?>
         <li><a href="">Home</a></li>
         <li><a href="">Programm</a></li>
         <li><a href="">Speeddating</a></li>
         <li><a href="">Tickets</a></li>
         <li><a href="">Projekte</a></li>
         <li><a href="">About</a></li>
+        <li><a href=""><img class="main-menu__search-icon" src="media/search-icon.png" alt="Search Icon"></a></li>
+        <?php 
+        if (!isSet($_SESSION['fhsUser'])) { ?> 
+            <li><a href="/auth/authorized.php">login</a></li>
+        <?php }else { ?>
+            <li><a href="/logout.php">logout</a></li>
+        <?php } ?>
     </ul>
     
 </nav>
