e9fe0d6b32611d51ce44a79c6c90d93c6c488c2f
Funiel <funiel@me.com>
2022-05-28 02:55:32 +0200

    About Page Desktop done

--- src/php/about.php
+++ src/php/about.php
@@ -123,7 +123,74 @@ require "components/head.php";
                 $person_role = "Kommunikation";
                 include "components/about_entry_small.php";
                 ?>
+                <div class="about-container__thanks-to__more">
+                    <p>DI(FH) Martin Ortner, Mag. Ingrid Langthaler, Mag. Hartwig Reiter, Gudrun Wieland, DI(FH) Michael
+                        Großauer, Bob Veltman, Nico Längauer, Tobias Türk, Magdalena Umkehrer, Christina Burgstaller,
+                        Jana Rowenski, Ricarda Moser, Anna Wirth, Anna Bruckermayr, Stefanie Aunitz, Valentina
+                        Lancellotti, Lea Kurz, Selina De Carlo, Jan Fischerlehner, Shanti Stangl, Kilian Kofler, Ellen
+                        Keßler, Lena Riedler, Marika Knöckl, Teo Crawford, Lena Könighofer, Christine Poplavski, Johanna
+                        Wicht, Nico Weiser, Daniel Wetzelhütter, Jan Reisecker, Lukas Stitz, Benjamin Fernerberger,
+                        Melanie Feigl, Lukas Pattermann, Jonathan Lindner, Phillip Law, Vincent Thierry, Martin Hartler,
+                        Elias Handschuh, Sarah Hartl, Lisa Grall, Jasmin Schlögl, Phillip Laa, Johanna Steiner,
+                        Alexander Mader, Nico Weiser</p>
+                </div>
                 <div class="about-container__thanks-to__filler"></div>
+            </div>
+            <div class="about-container__concept">
+                <div class="about-container__concept__heading">
+                    <h3>Konzept</h3>
+                </div>
+                <div class="about-container__concept__text">
+                    <p>
+                        Die vier unterschiedlichen Captcha Bilder, machen bei genauerer Betrachtung auf die
+                        Problemstellungen in unserer Welt aufmerksam. Auch MultiMediaArt, MultiMediaTechnology und
+                        Human-Computer Interaction beschäftigen sich neben dem Einsatz unterschiedlicher technischer
+                        Innovationen und neuen Medien vor allem auch mit aktuellen, kritischen Fragestellungen. Die
+                        subtil kritischen Captcha Bilder greifen dies auf, geben uns Menschen – im Gegensatz zu Robotern
+                        – einen Denkanstoß und berühren uns. Daher verifiziert man sich in einer interaktiven Animation,
+                        kein Roboter zu sein. In dieser Animation verschmelzen Technik und Konzept – das Captcha
+                        verschwindet und legt den Blick auf den verpixelten Rundgang-Schriftzug frei, der einlädt, an
+                        der zukunftsschaffenden Welt von MultiMediaArt, MultiMediaTechnology und Human-Computer
+                        Interaction teilhaben zu können.
+                    </p>
+                </div>
+                <div class="about-container__concept__captchas">
+                    <?php
+                    $captcha_image_path = "media/Captchas/Obdachlosigkeit.png";
+                    $captcha_message = "Select all squares <br>with palm trees.";
+                    $captcha_sideH = "left";
+                    $captcha_sideV = "top";
+                    $captcha_xH = 40;
+                    $captcha_xV = 20;
+                    include 'components/captcha.php';
+                    ?>
+                    <?php
+                    $captcha_image_path = "media/Captchas/Geschlechterrollen.png";
+                    $captcha_message = "Select all squares <br>with crosswalks.";
+                    $captcha_sideV = "buttom";
+                    $captcha_xH = 40;
+                    $captcha_xV = 20;
+                    include 'components/captcha.php';
+                    ?>
+                    <?php
+                    $captcha_image_path = "media/Captchas/Klimawandel.png";
+                    $captcha_message = "Select all squares <br>with cars.";
+                    include 'components/captcha.php';
+                    ?>
+                </div>
+            </div>
+            <div class="about-container__socials">
+                <img class="about-container__socials__notarobot"
+                    srcset="media/not-a-robot-px-borderd-v.png 193w, media/not-a-robot-px-borderd.png 278w"
+                    sizes="(max-width: 480px) 120px" alt="pixlated im not a robot signe">
+                <div class="about-container__socials__social-panel">
+                    <p>Folge Rundgang auf Social Media um nichts zu verpassen.</p>
+                    <div class="about-container__socials__social-panel__buttons">
+                        <a href="/soon.php"><button>Instagram</button></a>
+                        <a href="/soon.php"><button>Facebook</button></a>
+                        <a href="/soon.php"><button>TikTok</button></a>
+                    </div>
+                </div>
             </div>
         </div>
 
